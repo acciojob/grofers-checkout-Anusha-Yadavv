a42d9ffc51b6fa57af8f585c90addc06bbef5a62
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2023-12-21 16:06:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	let prices=document.querySelectorAll(".item .price");
+	let totalPrice=0;
+	prices.forEach((price)=>{
+		totalPrice+=parseInt(prices.innerText);
+	})
+	let tableElement=document.getElementById("itemsTable");
+	let newRow=tableElement.insertRow(-1);
+	let cell=newRow.insertCell(0);
+	cell.innerText=totalPrice;
   
 };
 
